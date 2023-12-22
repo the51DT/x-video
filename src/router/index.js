7ad6ac41c7329e01dcc41204b4daba708b7a8ec9
choiBom <bchoi@the-51.com>
2023-12-22 17:33:43 +0900

    update popupComp

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,55 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import home from "@/views/main/main.vue";
-import shots from "@/views/shots/Shots.vue";
+import home from '@/views/main/main.vue'
+import shots from '@/views/shots/Shots.vue'
+import popupLibrary from '@/views/shots/popupLibrary.vue'
+import popupSubscriptions from '@/views/shots/popupSubscriptions.vue'
+import popupUpdate from '@/views/shots/popupUpdate.vue'
 // import subscriptions from "@/views/shots/Subscriptions.vue";
 // import library from "@/views/shots/Library.vue";
 
-// 라우터 
+// 라우터
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: home,
-    }, 
-    {
-        path: "/shots",
-        name: "shots",
-        component: shots,
-    }, 
-    // {
-    //     path: "/subscriptions",
-    //     name: "subscriptions",
-    //     component: subscriptions,
-    // }, 
-    // {
-    //     path: "/library",
-    //     name: "library",
-    //     component: library,
-    // }
+  {
+    path: '/',
+    name: 'home',
+    component: home,
+  },
+  {
+    path: '/shots',
+    name: 'shots',
+    component: shots,
+  },
+  {
+    path: '/popupLibrary',
+    name: 'popupLibrary',
+    component: popupLibrary,
+  },
+  {
+    path: '/popupSubscriptions',
+    name: 'popupSubscriptions',
+    component: popupSubscriptions,
+  },
+  {
+    path: '/popupUpdate',
+    name: 'popupUpdate',
+    component: popupUpdate,
+  },
+  // {
+  //     path: "/subscriptions",
+  //     name: "subscriptions",
+  //     component: subscriptions,
+  // },
+  // {
+  //     path: "/library",
+  //     name: "library",
+  //     component: library,
+  // }
 ]
 // 라우터 생성
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
+  history: createWebHistory(),
+  routes,
+})
 
 // 라우터 추출 (main.js에서 import)
-export {router}
+export { router }

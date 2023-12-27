680bb904e59175ef269d57fb71138f3cfc80062c
Bae <jhbae@the-51.com>
2023-12-27 15:22:07 +0900

    라이브러리 작업 커밋

---
+++ src/layouts/components/libraryList.vue
@@ -0,0 +1,37 @@
+<template>
+  
+  <div class="video-list">
+    <div class="video" @mouseover="playVideo" @mouseout="pauseVideo">
+      <img
+        class="thumbnail"
+        :src="video.thumbnail"
+        alt="Video Thumbnail"
+      />
+      <video
+        class="video-player"
+        v-if="video.playing"
+        :src="video.url"
+        autoplay
+        @ended="pauseVideo"
+      ></video>
+      <div class="video-details">
+        <h2>{{ video.title }}</h2>
+        <p>{{ video.nickName }} &nbsp; • &nbsp; {{ video.statistics.play_count }} 회</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['video']);
+
+const playVideo = () => {
+  props.video.playing = true;
+};
+
+const pauseVideo = () => {
+  props.video.playing = false;
+};
+</script>
--- src/views/shots/popupSubscriptions.vue
+++ src/views/shots/popupSubscriptions.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="phone-container">
     <div class="phone">
-      <div class="screen">
-        <div class="video">
-          <swipe :videos="videoList"></swipe>
-        </div>
-      </div>
       <div class="navigation">
         <navigation></navigation>
       </div>
-      <componentPopup type="bottomLayer"></componentPopup>
+      <componentPopup type="full">
+        <div class="library-wrap">
+          <div class="profile">
+            <div class="icon-user">user icon</div>
+            <div class="user">
+              <p class="user-name">Username</p>
+              <p class="user-subscribers">420 thousand subscribers</p>
+              <p class="user-subscribers"></p>
+            </div>
+          </div>
+          <div class="video-list">
+          </div>
+        </div>
+      </componentPopup>
     </div>
   </div>
 </template>
 
 <script setup>
 import navigation from '@/layouts/components/Navigation.vue'
-import swipe from '@/layouts/components/Swipe.vue'
 import componentPopup from '@/layouts/components/componentPopup.vue'
-import VideoData from '@/layouts/components/videoList.js'
 import '@/assets/scss/shots.scss'
 import { ref } from 'vue'
-
-const videoList = ref(VideoData)
 </script>

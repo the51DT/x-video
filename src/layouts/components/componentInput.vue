d06663d186cae10f5d6433370550ba18e3ac310f
choiBom <bchoi@the-51.com>
2023-12-28 12:41:22 +0900

    update 'upload' page

---
+++ src/layouts/components/componentInput.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="update__form__item">
+    <label :for="labelForId" class="input-label">{{ labelTitle }}</label>
+    <div v-if="inputType == 'text'" class="input-box">
+      <input
+        v-model="props.vModel"
+        :type="vModel"
+        :placeholder="placeholder"
+        :id="labelForId"
+        class="input-box__input-text"
+        @change="$emit('update:modelValue', $event.target.value)"
+      />
+    </div>
+    <div v-if="inputType == 'select'" class="input-box">
+      <select
+        :name="name"
+        :id="labelForId"
+        title="영상 종류를 선택하세요"
+        class="input-box__input-select"
+        @change="$emit('update:modelValue', $event.target.value)"
+      >
+        <!-- <option disabled selected value="">please select file type</option>
+        <option value="Upload">Upload</option>
+        <option value="Link">Link</option> -->
+        <option disabled value="">please select file type</option>
+        <option v-for="item in props.options" :key="item" :value="item.value">
+          {{ item.title }}
+        </option>
+      </select>
+    </div>
+    <div v-if="inputType == 'file'" class="input-box">
+      <div class="input-box__container">
+        <input
+          :id="uploadFile"
+          :type="inputType"
+          :placeholder="upload"
+          class="input-box__input-text"
+          @change="uploadFile"
+        />
+      </div>
+      <!-- <div
+        class="input-box__container input-box__container--preview"
+      >
+        <span class="input-box__container--preview__text"></span>
+      </div> -->
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  vModel: {
+    type: String,
+    default: '',
+  },
+  inputType: {
+    type: String,
+    default: '',
+  },
+  labelTitle: {
+    type: String,
+    default: '',
+  },
+  name: {
+    type: String,
+    default: '',
+  },
+  labelForId: {
+    type: String,
+    default: '',
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  url: {
+    type: String,
+    default: '',
+  },
+  options: {
+    type: String,
+    default: '',
+  },
+  value: {
+    type: String,
+    default: '',
+  },
+  class: {
+    type: String,
+    default: '',
+  },
+  hasError: {
+    type: String,
+    default: '',
+  },
+})
+
+let emit = defineEmits(['update:modelValue'])
+
+const selectChange = (event) => {
+  console.log(event.target.value)
+}
+console.log('props', props.options)
+</script>
--- src/views/shots/Update.vue
+++ src/views/shots/Update.vue
@@ -4,109 +4,89 @@
       <div class="screen">
         <div class="update-wrap">
           <div class="update__form">
-            <div class="update__form__item">
-              <label for="type">Type</label>
-              <select
-                name=""
-                id="type"
-                title="영상 종류를 선택하세요"
-                @change="selectedOpt"
-              >
-                <option value="">Upload</option>
-                <option value="">Link</option>
-              </select>
-            </div>
-            <!-- type: upload  -->
-            <template>
-              <div class="update__form__item">
-                <label for="uploadFile">upload</label>
-                <div class="input-box">
-                  <div class="input-box__container">
-                    <input
-                      id="uploadFile"
-                      type="file"
-                      placeholder="upload"
-                      class="input-box__input-text"
-                    />
-                  </div>
-                  <div
-                    class="input-box__container input-box__container--preview"
-                  >
-                    <span class="input-box__container--preview__text"></span>
-                  </div>
-                </div>
-              </div>
-              <updateInputText
-                labelTitle="URL"
-                labelFor="url"
-                placeholder="URL"
-                url="test"
-                class=""
+            <componentInput
+              v-model="vSelected"
+              inputType="select"
+              labelTitle="Type"
+              labelForId="type"
+              placeholder="Type"
+              name="type"
+              :options="addedSelectOptions"
+              @update:modelValue="changeSelect"
+            />
+            <template
+              v-if="vSelected == 'upload'"
+              :class="vSelected == 'upload' ? 'show' : 'hide'"
+            >
+              <!-- type: upload template -->
+              <componentInput
+                v-model="vupload"
+                inputType="file"
+                labelTitle="Upload"
+                labelForId="upload"
+                placeholder="upload"
+                name="upload"
               />
-              <!-- <div class="update__form__item">
-                <label for="title">URL</label>
-                <div class="input-box">
-                  <input
-                    type="text"
-                    placeholder="URL"
-                    id="url"
-                    class="input-box__input-text"
-                  />
-                </div>
-              </div> -->
             </template>
 
-            <!-- type: youtube  -->
-            <template>
-              <div class="update__form__item">
-                <label for="title">youtube</label>
-                <div class="input-box">
-                  <input
-                    type="text"
-                    placeholder="URL"
-                    id="url"
-                    class="input-box__input-text"
-                  />
-                </div>
-              </div>
+            <!-- type: youtube template -->
+            <template
+              v-if="vSelected == 'link'"
+              :class="vSelected == 'link' ? 'show' : 'hide'"
+            >
+              <!--youtube  -->
+              <componentInput
+                v-model="vtext"
+                inputType="text"
+                labelTitle="youtube"
+                labelForId="youtube"
+                placeholder="URL"
+                name="youtube"
+              />
             </template>
 
-            <div class="update__form__item">
-              <label for="title">Title</label>
-              <div class="input-box">
-                <input
-                  type="text"
-                  placeholder="Title"
-                  id="title"
-                  class="input-box__input-text"
-                />
-              </div>
-            </div>
-            <div class="update__form__item">
-              <label for="title">Description</label>
-              <div class="input-box">
-                <input
-                  type="text"
-                  placeholder="Description"
-                  id="description"
-                  class="input-box__input-text"
-                />
-              </div>
-            </div>
+            <!-- Title -->
+            <componentInput
+              v-model="vTitle"
+              inputType="text"
+              labelTitle="Title"
+              labelForId="title"
+              placeholder="Title"
+              name="title"
+              @update:modelValue="changeTitle"
+            />
+            <!-- Description -->
+            <componentInput
+              v-model="vDescription"
+              inputType="text"
+              labelTitle="Description"
+              labelForId="description"
+              placeholder="Description"
+              name="description"
+              @update:modelValue="changeDescription"
+            />
 
             <div class="update__form__btn-wrap">
               <button
                 type="butotn"
                 class="update__form__button update__form__button--submit"
+                @click="saveData"
               >
                 <span class="update__form__button__text">Upload</span>
               </button>
-              <button
-                type="butotn"
+
+              <router-link
+                to="/home"
                 class="update__form__button update__form__button--cancel"
               >
                 <span class="update__form__button__text">Cancel</span>
-              </button>
+                <!-- <button
+                  type="butotn"
+                  class="update__form__button update__form__button--cancel"
+                >
+                  <span class="update__form__button__text">Cancel</span>
+                </button> -->
+              </router-link>
             </div>
           </div>
         </div>
@@ -123,15 +103,37 @@
 
 <script setup>
 import navigation from '@/layouts/components/Navigation.vue'
-import updateInputText from '@/layouts/components/updateInputText.vue'
+import componentInput from '@/layouts/components/componentInput.vue'
 import VideoData from '@/layouts/components/videoList.js'
 import '@/assets/scss/index.scss'
 import '@/assets/scss/update.scss'
 import { ref } from 'vue'
 
+const vSelected = ref('link')
+const vTitle = ref()
+const vDescription = ref()
 const videoList = ref(VideoData)
-const addedList = ref([])
+const addedSelectOptions = ref([
+  {
+    title: 'Link',
+    value: 'link',
+  },
+  {
+    title: 'Upload',
+    value: 'upload',
+  },
+])
+const changeSelect = () => {
+  console.log('changeSelect', event.target.value, vSelected.value)
+}
 
+const changeTitle = () => {
+  console.log('changeTitle', event.target.value, vTitle.value)
+}
+
+const changeDescription = () => {
+  console.log('changeDescription', event.target.value, vDescription.value)
+}
 // title  특문x, 2글자 이상
 // 첨부 -> pass
 </script>
